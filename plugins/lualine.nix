e89578b8abe0cf0858bc8a4fa976bfd5a87f1f6c
Narendra S <narendra.s1232@gmail.com>
2025-09-23 12:28:55 +0530

    remove import from lualine

--- plugins/lualine.nix
+++ plugins/lualine.nix
@@ -43,10 +43,4 @@
         };
       };
   };
-  imports =
-    with builtins;
-    with lib;
-    map (fn: ./${fn}) (
-      filter (fn: (fn != "default.nix" && !hasSuffix ".md" "${fn}")) (attrNames (readDir ./.))
-    );
 }
